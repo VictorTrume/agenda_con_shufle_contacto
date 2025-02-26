5d3f684565ed1536a170d079d8e19185aa54aaa8
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2025-02-26 09:46:44 -0700

    [INICIALIZA] el proyecto de xcode

---
+++ Agenda Shufle/Agenda Shufle/Agenda_ShufleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agenda_ShufleApp.swift
+//  Agenda Shufle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Agenda_ShufleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PantallaAgenda()
+        }
+    }
+}
---
+++ Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
@@ -0,0 +1,18 @@
+//
+//  PantallaAgenda.swift
+//  Agenda Shufle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct PantallaAgenda: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PantallaAgenda()
+}
